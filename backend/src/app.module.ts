039088f51db05232a763a3bd6b12710d3554b8c5
LT-Ripjaws <nightangle676x@gmail.com>
2026-06-08 14:45:27 +0600

    mongoose and docker setup

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,30 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    MongooseModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => {
+        const uri = configService.get<string>('MONGODB_URI');
+
+        if (!uri) {
+          throw new Error('MONGODB_URI is not set');
+        }
+
+        return {
+          uri,
+          serverSelectionTimeoutMS: 5000,
+        };
+      },
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
